bbe57d723784558186510a8f3dd8fd92a678f68f
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2025-02-12 18:20:07 +0000

    Add files via upload

---
+++ Scripts/download_data.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+wget https://ftp.ebi.ac.uk/pub/databases/cryptic/release_june2022/reuse/CRyPTIC_excluded_samples_20220607.tsv
+wget https://ftp.ebi.ac.uk/pub/databases/cryptic/release_june2022/reuse/CRyPTIC_reuse_table_20221019.csv
+wget https://ftp.ebi.ac.uk/pub/databases/cryptic/release_june2022/reuse/00/01/08/61/10861/site.02.iso.1.subject.0001.lab_id.2014222001.seq_reps.14222001.masked.vcf.gz
