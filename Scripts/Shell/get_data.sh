4905cbfdd5ec797a1663ba3248bfea24cc487a69
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2025-03-11 16:40:07 +0000

    Delete Scripts directory

--- Scripts/Shell/get_data.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-wget https://ftp.ebi.ac.uk/pub/databases/cryptic/release_june2022/reuse/CRyPTIC_reuse_table_20221019.csv
-wget https://ftp.ebi.ac.uk/pub/databases/cryptic/release_june2022/reuse/CRyPTIC_reuse_table_20231027.csv
-wget https://ftp.ebi.ac.uk/pub/databases/cryptic/release_june2022/reuse/CRyPTIC_reuse_table_20231107.csv
-wget https://ftp.ebi.ac.uk/pub/databases/cryptic/release_june2022/reuse/CRyPTIC_reuse_table_20231208.csv
-wget https://ftp.ebi.ac.uk/pub/databases/cryptic/release_june2022/reuse/CRyPTIC_reuse_table_20240917.csv
-
-echo "Data = downloaded!"
